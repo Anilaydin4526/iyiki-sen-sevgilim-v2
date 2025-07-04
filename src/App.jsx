57e00ec5d776b4e095cd31c4b94a008345863c4c
Anıl Aydın <anianilar4@gmail.com>
2025-07-04 10:50:41 +0300

    App.css ve App.jsx dosyaları eklendi/güncellendik.

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@
         {admin ? "Siteye Dön" : "Yönetici Girişi"}
       </button>
       <div className="header-center">
-        <div className="heart-animation">❤️</div>
-        <h1 className="main-title">İyiki Sen Sevgilim</h1>
-        <p className="subtitle">
+        <center><div className="heart-animation">❤️</div></center>
+       <center> <h1 className="main-title">İyiki Sen Sevgilim</h1></center>
+       <center> <p className="subtitle">
           Birlikte geçirdiğimiz her an, bu sitede sonsuza dek yaşayacak...
-        </p>
+        </p></center>
       </div>
       <div className="welcome-message">
-        <p>
+        <center><p>
           Hoşgeldin! Bu site, aşkımızın hikayesini, anılarımızı ve sürprizleriyle dolu bir yolculuğu anlatıyor. Her tıklamada yeni bir sürpriz, her sayfada yeni bir anı seni bekliyor. Hazır mısın?
-        </p>
+        </p></center>
       </div>
       <ParallaxBanner />
       {admin ? (
