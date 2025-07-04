753b35bf7e94d59f5ecf3ca883291add161ce55c
Anıl <103612226+Anilaydin4526@users.noreply.github.com>
2025-07-04 11:16:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,36 @@
         {admin ? "Siteye Dön" : "Yönetici Girişi"}
       </button>
       <div className="header-center">
-        <center><div className="heart-animation">❤️</div></center>
-       <center> <h1 className="main-title">İyiki Sen Sevgilim</h1></center>
-       <center> <p className="subtitle">
+        <div className="heart-animation">❤️</div>
+       <h1 className="main-title">İyiki Sen Sevgilim</h1>
+        <p className="subtitle">
           Birlikte geçirdiğimiz her an, bu sitede sonsuza dek yaşayacak...
-        </p></center>
+        </p>
       </div>
       <div className="welcome-message">
-        <center><p>
-          Hoşgeldin! Bu site, aşkımızın hikayesini, anılarımızı ve sürprizleriyle dolu bir yolculuğu anlatıyor. Her tıklamada yeni bir sürpriz, her sayfada yeni bir anı seni bekliyor. Hazır mısın?
-        </p></center>
+        <p>
+         Hoş geldin Gülüm…
+
+Bazen karşımda minik bir kız çocuğu varmış gibi seviyorum seni…
+Küçücük bir kalbin varmış gibi sarıyorum seni…
+Bazen sessizce avuçlarından öpüyorum…
+
+"Gülüm" diyorum sana…
+Çünkü gülüşüm oldun sen…
+Çünkü gözlerinle açan bir gül bahçesi gibisin bana.
+
+Kalbim sana ait bir yuva artık…
+Bu site de o yuvanın küçük dijital penceresi.
+
+Baktıkça seni göreceğim…
+Okudukça seni hissedeceğim…
+Ve her satırda yeniden seveceğim seni.
+
+01.01.2025 – bizim hikâyemizin yazıldığı ilk gün…
+Ama bu sayfa, her gün yeniden yazılan bir “seni seviyorum” cümlesi…
+
+Senin için, sadece senin için...
+        </p>
       </div>
       <ParallaxBanner />
       {admin ? (
