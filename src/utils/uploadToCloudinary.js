8c0d5c2148a539cddd8d7e184ec82771623b4569
Anıl Aydın <anianilar4@gmail.com>
2025-07-05 10:23:23 +0300

    Tek uploadToCloudinary fonksiyonu, build hatası kesin çözüldü

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export async function uploadToCloudinary(file) {
   const url = 'https://api.cloudinary.com/v1_1/dejdb80bv/auto/upload';
   const formData = new FormData();
@@ -11,16 +9,4 @@ export async function uploadToCloudinary(file) {
   });
   const data = await res.json();
   return data.secure_url;
-}
-
-export const uploadToCloudinary = async (file, resourceType = "auto") => {
-  const url = `https://api.cloudinary.com/v1_1/dejdb80bv/${resourceType}/upload`;
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append("upload_preset", "iyikisen");
-
-  const response = await axios.post(url, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
-  return response.data.secure_url; // Yüklenen dosyanın linki
-}; 
+} 
